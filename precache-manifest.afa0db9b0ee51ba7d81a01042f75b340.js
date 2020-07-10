6a4e92a37ff0df0637d7f623f3f7a871697d44fe
ErickDelRey <erick.del.rey5@gmail.com>
2020-07-10 23:56:19 +0800

    Updates

---
+++ precache-manifest.afa0db9b0ee51ba7d81a01042f75b340.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cc3c80003d88c7ccdf40187551d3c58",
+    "url": "/react-pics/index.html"
+  },
+  {
+    "revision": "e33d29e69669432efcf3",
+    "url": "/react-pics/static/js/2.58488f5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pics/static/js/2.58488f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ce3710f63032bbc5a01",
+    "url": "/react-pics/static/js/main.0c328721.chunk.js"
+  },
+  {
+    "revision": "f93e97bad7c76fe80bf7",
+    "url": "/react-pics/static/js/runtime-main.2abc479c.js"
+  }
+]);
